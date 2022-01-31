a7e97c1421afdbfeca5605467b8f5560d0aeabd6
JannaCha <98767007+JannaCha@users.noreply.github.com>
2022-01-31 21:04:52 +0300

    Delete Task_1 directory

--- Task_1/ALTER.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE Quantity ADD FOREIGN KEY (goods_id) REFERENCES Goods (id);
-ALTER TABLE Goods ADD FOREIGN KEY (supplier_id) REFERENCES Suppliers (id);
-ALTER TABLE Prices ADD FOREIGN KEY (goods_id) REFERENCES Goods (id);
-ALTER TABLE Suppliers ADD FOREIGN KEY (manufacturer_id) REFERENCES Manufacturer (id);
